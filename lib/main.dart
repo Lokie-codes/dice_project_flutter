8c539a7376d1cb3a6a5febaf968835f1456f48c3
Lokesh S <lokeshlucky13579@gmail.com>
2023-02-15 20:40:20 +0530

    addition of row child images

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: const Text('Role Dice 🎲'),
-          
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -19,6 +18,19 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Image.asset(
+            'images/dice1.png',
+          ),
+        ),
+        Expanded(
+          child: Image.asset(
+            'images/dice2.png',
+          ),
+        ),
+      ],
+    );
   }
 }
